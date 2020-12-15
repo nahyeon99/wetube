6d30a1a89df5c108f9f0636f2892ed1c5e7a58eb
NaHyeon <nahyeonee99@gmail.com>
2020-12-15 20:08:58 +0900

    #2.17

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,14 +5,14 @@ export const login = (req, res) => {
     res.send("Login");
 }
 export const logout = (req, res) => {
-    res.render("logout")
+    res.render("logout", { pageTitle: "Logout" })
 }
 export const userDetail = (req, res) => {
-    res.render("userDetail");
+    res.render("userDetail", { pageTitle: "User Detail" });
 }
 export const editProfile = (req, res) => {
-    res.render("editProfile");
+    res.render("editProfile", { pageTitle: "Edit Profile" });
 }
 export const changePassword = (req, res) => {
-    res.render("changePassword");
+    res.render("changePassword", { pageTitle: "Change Password" });
 }
