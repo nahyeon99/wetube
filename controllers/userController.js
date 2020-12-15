1991b9eb7baa3a0c0fcd44ad972f85d41ec67a38
NaHyeon <nahyeonee99@gmail.com>
2020-12-15 19:02:20 +0900

    #2.12

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+export const join = (req, res) => {
+    res.send("Join");
+}
+export const login = (req, res) => {
+    res.send("Login");
+}
+export const logout = (req, res) => {
+    return res.send("Logout");
+}
+
+export const users = (req, res) => {
+    return res.send("Users");
+}
+export const userDetail = (req, res) => {
+    res.send("User Detail");
+}
+export const editProfile = (req, res) => {
+    res.send("Edit Profile");
+}
+export const changePassword = (req, res) => {
+    res.send("Change Password");
+}
