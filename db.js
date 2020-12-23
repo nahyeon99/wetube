1ffa76a218cd3e0b9160615ca3faf537257bd70e
NaHyeon <nahyeonee99@gmail.com>
2020-12-24 02:04:28 +0900

    #3.4

--- db.js
+++ db.js
@@ -1,50 +1,26 @@
-export const videos = [
-    {
-        id: 324393,
-        title: "Video awesome",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-        creator: {
-            id: 121212,
-            name: "Nahyeon",
-            email: "nah@yeon.com"
-        }
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(
+    /* mongod msg error add code */
+    "mongodb://127.0.0.1:27017/whatever",{
+    ignoreUndefined: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
     },
+    /******************************/
+    process.env.MONGO_URL,
     {
-        id: 324222,
-        title: "Video beautiful",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-        creator: {
-            id: 121212,
-            name: "Nahyeon",
-            email: "nah@yeon.com"
-        }
-    },
-    {
-        id: 324111,
-        title: "Video nice",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-        creator: {
-            id: 121212,
-            name: "Nahyeon",
-            email: "nah@yeon.com"
-        }
-    },
-    {
-        id: 324394,
-        title: "Video super",
-        description: "This is something I love",
-        views: 24,
-        videoFile: "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4",
-        creator: {
-            id: 121212,
-            name: "Nahyeon",
-            email: "nah@yeon.com"
-        }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = (error) => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
