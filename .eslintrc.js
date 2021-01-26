4b5311eb084c9d647b1a29b607849f1cd8b76140
NaHyeon <nahyeonee99@gmail.com>
2021-01-27 00:44:57 +0900

    #8.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-    "no-console": "off",
-    "no-else-return": "off",
+    'no-console': 'off',
+    'no-else-return': 'off',
+  },
+  env: {
+    browser: true,
   },
 };
--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,46 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 const videoPlayer = videoContainer.querySelector('video');
 const playBtn = document.getElementById('jsPlayButton');
+const volumnBtn = document.getElementById('jsVolumnBtn');
+const fullScrnBtn = document.getElementById('jsFullScreen');
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
+}
+
+function handleVolumnClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumnBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
+
+function exitFullScreen() {
+  fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
+  fullScrnBtn.addEventListener('click', goFullScreen);
+  document.webkitExitFullscreen();
+}
+
+function goFullScreen() {
+  videoContainer.requestFullscreen();
+  fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
+  fullScrnBtn.removeEventListener('click', goFullScreen);
+  fullScrnBtn.addEventListener('click', exitFullScreen);
 }
 
 function init() {
   playBtn.addEventListener('click', handlePlayClick);
+  volumnBtn.addEventListener('click', handleVolumnClick);
+  fullScrnBtn.addEventListener('click', goFullScreen);
 }
 
 if (videoContainer) {
