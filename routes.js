ff64e58b1e30728b2d20fb7de69f5bd279cf35e4
NaHyeon <nahyeonee99@gmail.com>
2021-02-19 23:24:25 +0900

    #10

--- routes.js
+++ routes.js
@@ -24,6 +24,11 @@ const DELETE_VIDEO = '/:id/delete';
 const GITHUB = '/auth/github';
 const GITHUB_CALLBACK = '/auth/github/callback';
 
+// API
+const API = '/api';
+const REGISTER_VIEW = '/:id/view';
+const ADD_COMMENT = '/:id/comment';
+
 // Facebook
 const FB = '/auth/facebook';
 const FB_CALLBACK = 'auth/facebook/callback';
@@ -72,6 +77,9 @@ const routes = {
   me: ME,
   facebook: FB,
   facebookCallback: FB_CALLBACK,
+  api: API,
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT,
 };
 
 export default routes;
